3f0cfef5dcf247e0126c6ffd8ba818320cd271df
ishaq-p <sultan.ishaq.321@gmail.com>
2023-05-31 01:16:03 +0300

    lagrange and num_diff added

---
+++ Numerical_Analysis/lagrange_poly.py
@@ -0,0 +1,33 @@
+from rounding import rounding as rnd
+
+def RE(p1,p0):
+    return abs((p1-p0))/abs(p1)
+
+
+def L_(xx,x):
+    n=len(x)
+    L=[]
+
+    for i in range(n):
+        L.append(1)
+        for j in range(n):
+            if i!=j:
+                L[i] *= (xx-x[j])/(x[i]-x[j])
+    return L
+
+
+def lagrange_poly(x,y,xx, f_x, sig_digits):
+    result=[]
+    n=len(x)
+    L_i=L_(xx,x)
+    L=0
+    m=1
+
+    result.append(('(n)', '(x)', '(y)', '(L)', '(Li*yi)'))
+    for i in range(n):
+        L += L_i[i]*y[i]
+        result.append((m, x[i], y[i], L_i[i], L_i[i]*y[i]))
+        m+=1
+
+    result.append((f"L({xx}) = {round(rnd(L, sig_digits)[-1], 15)}", f"RE = {RE(f_x,L)}"))
+    return result
