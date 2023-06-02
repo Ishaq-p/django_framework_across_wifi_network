960619958e9a80af364d157e6ac35f1faf5c7f2b
ishaq-p <sultan.ishaq.321@gmail.com>
2023-06-02 23:42:51 +0300

    favicon added (code)

--- myapp/views.py
+++ myapp/views.py
@@ -48,7 +48,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('tic_tac_toe')
+            return redirect('home')
         else:
             return render(request, 'login.html', {'error_message': 'Invalid login credentials'})
     else:
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,6 +19,9 @@ from myapp.views import *
 from django.contrib import admin
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -38,5 +41,5 @@ urlpatterns = [
     path('N_Analysis/', include('Numerical_Analysis.urls')),
 
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
